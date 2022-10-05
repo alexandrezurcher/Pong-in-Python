7b8b9b5a3a157173a602b20d5a8bbf014e7943ec
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-05 20:07:04 +0100

    added the ball class create a ball object and make it moove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800,height=600)
@@ -7,9 +9,9 @@ screen.bgcolor("black")
 screen.title("Ping Pong")
 screen.tracer(0)
 
-
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350,0))
+ball = Ball()
 
 screen.listen()
 
@@ -20,6 +22,8 @@ screen.onkey(fun=l_paddle.go_down, key="s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
