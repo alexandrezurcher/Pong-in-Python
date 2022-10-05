b3601e2b3a6de957550f0d3634d850dba391badf
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-05 19:08:55 +0100

    create a paddle class and athe other paddle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import Screen, Turtle
-
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800,height=600)
@@ -7,24 +7,16 @@ screen.bgcolor("black")
 screen.title("Ping Pong")
 screen.tracer(0)
 
-paddle = Turtle("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.setpos(350, 0)
 
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350,0))
 
 screen.listen()
 
-screen.onkey(fun=go_up, key="Up")
-screen.onkey(fun=go_down, key="Down")
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
 
 game_is_on = True
 while game_is_on:
